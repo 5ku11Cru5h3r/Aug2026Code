515c50964dac756f2235fe48f38ca7b9eb5be27e
DBDA82 <DBDA82@ACTS.CDACECBLR.COM>
2026-09-02 18:12:14 +0530

    2-9-2026 18:12

---
+++ Day06.py
@@ -0,0 +1,58 @@
+class SmartThermostat:
+    MIN_TEMP = 10.0
+    MAX_TEMP = 35.0
+
+    def __init__(self, appliance_name: float, initial_temp: float):
+        self.__appliance_name = appliance_name
+        self.__target_temp = initial_temp
+        pass
+
+    @property
+    def target_temp(self):
+        return self.__target_temp
+
+    @target_temp.setter
+    def target_temp(self, temperature):
+        if (isinstance(temperature, (float, int)) == False):
+            raise TypeError("Give a float or int in temperature")
+        if (SmartThermostat.MAX_TEMP < temperature or SmartThermostat.MIN_TEMP > temperature):
+            self.__target_temp = 22.0
+            raise ValueError(
+                f"`initial_temp` is out of the `[{SmartThermostat.MIN_TEMP, SmartThermostat.MAX_TEMP}]` bounds, it defaults to `22.0`")
+        self.__target_temp = temperature
+
+    @property
+    def appliance_name(self):
+        return self.__appliance_name
+
+    # @appliance_name.setter
+    # def appliance_name(self, *args):
+    #     self.__appliance_name = args[0]
+
+
+class Vehicle:
+    def __init__(self,make:str,model:str,fuel_capacity:float):
+        self.appliance_name = appliance_name
+        self.appliance_name = appliance_name
+        self.appliance_name = appliance_name
+        pass
+    def calculate_range(self,fuel_efficiency):
+
+class DeliveryTruck(Vehicle):
+
+
+def main():
+    thermostat = SmartThermostat("Living Room AC", 24.0)
+    print(thermostat.appliance_name)  # Output: Living Room AC
+    print(thermostat.target_temp)     # Output: 24.0
+
+    thermostat.target_temp = 28.0     # Updates successfully
+    print(thermostat.target_temp)     # Output: 28.0
+
+    try:
+        thermostat.target_temp = 5.0  # Out of range!
+    except ValueError as e:
+        print(e)  # Output: Temperature must be between 10.0 and 35.0 degrees.
+
+
+main()
